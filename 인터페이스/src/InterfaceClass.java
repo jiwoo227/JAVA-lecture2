18e13a9a2b4dbbc0244d6a71207590a7310fbcf1
최지우 <w2040@e-mirim.hs.kr>
2022-09-03 23:02:06 +0900

    인터페이스

---
+++ 인터페이스/src/InterfaceClass.java
@@ -0,0 +1,15 @@
+public class InterfaceClass implements InterfaceA, InterfaceB{
+
+    public InterfaceClass(){
+        System.out.println("InterfaceClass");
+    }
+    @Override
+    public void funA() {
+        System.out.println("funA");
+    }
+
+    @Override
+    public void funB() {
+        System.out.println("funB");
+    }
+}
