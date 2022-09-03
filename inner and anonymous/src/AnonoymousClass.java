a2779203cbc02a091842b7244f9a9e7377c22e92
최지우 <w2040@e-mirim.hs.kr>
2022-09-03 22:33:56 +0900

    내부 메소드와 익명 메소드

---
+++ inner and anonymous/src/AnonoymousClass.java
@@ -0,0 +1,8 @@
+public class AnonoymousClass {
+        public AnonoymousClass() {
+            System.out.println("AnonymousClass constructor");
+        }
+        public void method() {
+            System.out.println(" -- AnonymousClass's method START -- ");
+        }
+    }
---
+++ inner and anonymous/src/MainClass.java
@@ -0,0 +1,25 @@
+public class MainClass {
+    public static void main(String[] args) {
+        OuterClass oc = new OuterClass();
+        System.out.println("oc.num : " + oc.num);
+        System.out.println("oc.str1 : " + oc.str1);
+        System.out.println();
+// 멤버 inner 클래스
+        OuterClass.InnerClass in = oc.new InnerClass();
+        System.out.println("in.num : " + in.num);
+        System.out.println("in.str2 : " + in.str2);
+        System.out.println();
+// static inner 클래스
+        OuterClass.SInnerClass si = new OuterClass.SInnerClass();
+        System.out.println("si.num : " + si.num);
+        System.out.println("si.str3 : " + si.str3);
+        System.out.println();
+
+        new AnonoymousClass() {
+            @Override
+            public void method() {
+                System.out.println(" -- AnonymousClass's Override method START -- ");
+            };
+        }.method();
+    }
+}
